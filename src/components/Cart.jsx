9df797cb24b0eb7d88aaff35f85a22015162d8d4
Vladimir Khan <vencedor843@gmail.com>
2024-11-10 02:15:38 +0900

    first commit

---
+++ src/components/Cart.jsx
@@ -0,0 +1,17 @@
+import { CartItem } from "./CartItem";
+export function Cart({ cartItems }) {
+  return (
+    <>
+      <h2 className="mb-5 text-4xl font-bold dark:text-white">
+        Cart
+      </h2>
+      <ul className="space-y-5">
+        {cartItems.map((cartItem) => (
+          <li key={cartItem.product.id}>
+            <CartItem item={cartItem} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
